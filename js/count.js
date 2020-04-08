8cff7e3f3e6ed102c86dc652c272341a545ea48d
Mr-Ojii <okaschan@gmail.com>
2020-04-08 23:12:09 +0900

    記事追加、JS追加、CSS変更

---
+++ js/count.js
@@ -0,0 +1,7 @@
+﻿window.onload = function () {
+    var element = document.getElementById('side_2020-s');
+    var element2 = document.getElementById('side_2020');
+    var hyoujimoji2020 = '▼2020(' + $('#s_2020-s a').length+ ')';
+    element.innerHTML = hyoujimoji2020;
+    element2.innerHTML = hyoujimoji2020;
+};
